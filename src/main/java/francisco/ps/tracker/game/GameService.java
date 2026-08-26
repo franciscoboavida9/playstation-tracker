7d12b872ebb0957ace54fa4c424b96416ac93ee6
Francisco Boavida <franciscoboavida9@gmail.com>
2026-08-26 20:51:26 +0100

    search feature: dto, adapter and adapter test

--- src/main/java/francisco/ps/tracker/game/GameService.java
+++ src/main/java/francisco/ps/tracker/game/GameService.java
@@ -1,9 +1,6 @@
 package francisco.ps.tracker.game;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-
-import java.net.URI;
 
 @Service
 public class GameService {
---
+++ src/main/java/francisco/ps/tracker/infrastructure/sony/dto/SearchResponseDto.java
@@ -0,0 +1,27 @@
+package francisco.ps.tracker.infrastructure.sony.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SearchResponseDto(
+        DataDto data
+) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record DataDto(
+       @JsonProperty("universalSearch") SearchDto search
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record SearchDto(
+            List<ResultDto> results
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record ResultDto(
+            String id,
+            String name
+    ) {}
+}
