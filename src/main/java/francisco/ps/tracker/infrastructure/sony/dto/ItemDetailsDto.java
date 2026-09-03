d47d9d636cf2b37b5daaedf042e0b60d8901b7d3
Francisco Boavida <franciscoboavida9@gmail.com>
2026-09-03 12:31:06 +0100

    complete item service, moving on to tracker

--- src/main/java/francisco/ps/tracker/infrastructure/sony/dto/ItemDetailsDto.java
+++ src/main/java/francisco/ps/tracker/infrastructure/sony/dto/ItemDetailsDto.java
@@ -1,4 +1,8 @@
 package francisco.ps.tracker.infrastructure.sony.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ItemDetailsDto {
+
 }
